9000add2128157ae8c9665794e40d1ab36121366
EscapesRooms <carlostisairejurad@gmail.com>
2026-08-20 13:04:48 +0200

    Add team selection functionality in game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,30 @@
+let selectedTeam = "";
 
+const teamButtons = document.querySelectorAll(".team");
+const continueBtn = document.getElementById("continueBtn");
+const selectedTeamText = document.getElementById("selectedTeam");
+
+teamButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        selectedTeam = button.dataset.team;
+
+        selectedTeamText.innerHTML =
+            "Equipo seleccionado: <strong>" +
+            selectedTeam +
+            "</strong>";
+
+        continueBtn.disabled = false;
+
+        localStorage.setItem("team", selectedTeam);
+
+    });
+
+});
+
+continueBtn.addEventListener("click", () => {
+
+    alert("Perfecto. Más adelante irá a la pantalla de reglas.");
+
+});
