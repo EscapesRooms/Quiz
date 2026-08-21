4bd42d2bb0a3aa91dccd0f6ad40aed66a611a682
EscapesRooms <carlostisairejurad@gmail.com>
2026-08-21 15:24:01 +0200

    Implement player creation in Firestore

--- js/game-firebase-test.js
+++ js/game-firebase-test.js
@@ -1,1 +1,55 @@
+/* ==========================================
+   IMPORTAR SERVICIO DE JUGADORES
+   ========================================== */
 
+import {
+    savePlayer
+}
+from "./player-service.js";
+
+
+/* ==========================================
+   CREAR JUGADOR EN FIRESTORE
+   ========================================== */
+
+document
+.getElementById("conti*ueBtn")
+.addEventListener(
+    "cl*ck",
+    async () => {
+
+        tr*{
+
+            // Crear un ID únic*
+            const playerId =
+    *       crypto.randomUUID();
+
+     *      // Guardar jugador
+         *  await savePlayer(
+
+             *  playerId,
+                "Carlo*",
+                "Verde",
+      *         0
+
+            );
+
+      *     alert(
+
+                "Juga*or creado:\n\n" +
+                *layerId
+
+            );
+
+        }*        catch(error){
+
+           *console.error(error);
+
+           *alert(
+                "Error guar*ando jugador"
+            );
+
+    *   }
+
+    }
+);
