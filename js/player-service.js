9c30da2b4a83eab3f6e8bb2e933a55e6a1a81ebb
EscapesRooms <carlostisairejurad@gmail.com>
2026-08-21 15:12:35 +0200

    Implement savePlayer function for player management
    Added savePlayer function to create or update player data in Firestore.

--- js/player-service.js
+++ js/player-service.js
@@ -1,1 +1,43 @@
+/* ==========================================
+   SERVICIO DE JUGADORES
+   ========================================== */
 
+import {
+    db,
+    doc,
+    setDoc
+}
+from "./firebase.js";
+
+
+/* ==========================================
+   CREAR O ACTUALIZAR JUGADOR
+   ========================================== */
+
+export async function savePlayer(
+
+    playerId,
+    playerName,
+    team,
+    score
+
+){
+
+    await setDoc(
+
+        doc(
+            db,
+            "players",
+            playerId
+        ),
+
+        {
+            playerId,
+            playerName,
+            team,
+            score
+        }
+
+    );
+
+}
